bcda21eb9cdcc463f0daf6e58d0f93f1103ab5ac
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-11-30 08:59:18 +0100

    cmake: armclang prototyping
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -1517,8 +1517,10 @@ function(check_compiler_flag lang option ok)
 
   if(${${${ok}}})
     set(ret 1)
+    message("Checking ${option}; result: 1")
   else()
     set(ret 0)
+    message("Checking ${option}; result: 0")
   endif()
 
   set(${ok} ${ret} PARENT_SCOPE)
