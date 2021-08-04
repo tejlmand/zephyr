c00e9168ef58e40cabd05d876647b93ff7b1fc10
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-08-04 10:57:39 +0200

    squash! squash! cmake: linker: converter arm and common ld scripts into CMake code
    Updated to use linker groups.

--- cmake/linker_script/common/common-rom.cmake
+++ cmake/linker_script/common/common-rom.cmake
@@ -1,6 +1,6 @@
 # originates from common-rom.ld
 
-zephyr_linker_section(NAME init KVMA ${RAM_REGION} LMA ${ROM_REGION})
+zephyr_linker_section(NAME init KVMA RAM_REGION LMA ROM_REGION)
 zephyr_linker_section_obj_level(SECTION init LEVEL PRE_KERNEL_1)
 zephyr_linker_section_obj_level(SECTION init LEVEL PRE_KERNEL_2)
 zephyr_linker_section_obj_level(SECTION init LEVEL POST_KERNEL)
@@ -9,21 +9,21 @@ zephyr_linker_section_obj_level(SECTION init LEVEL SMP)
 
 if(CONFIG_GEN_SW_ISR_TABLE AND NOT CONFIG_DYNAMIC_INTERRUPTS)
   # ld align has been changed to subalign to provide identical behavior scatter vs. ld.
-  zephyr_linker_section(NAME sw_isr_table KVMA FLASH LMA ${ROM_REGION} SUBALIGN ${CONFIG_ARCH_SW_ISR_TABLE_ALIGN} NOINPUT)
+  zephyr_linker_section(NAME sw_isr_table KVMA FLASH LMA ROM_REGION SUBALIGN ${CONFIG_ARCH_SW_ISR_TABLE_ALIGN} NOINPUT)
   zephyr_linker_section_configure(
     SECTION sw_isr_table
     INPUT ".gnu.linkonce.sw_isr_table*"
   )
 endif()
 
-zephyr_linker_section(NAME initlevel_error KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME initlevel_error KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION initlevel_error INPUT ".init_[_A-Z0-9]*" KEEP SORT NAME)
 # How to do cross linker ?
 # ASSERT(SIZEOF(initlevel_error) == 0, "Undefined initialization levels used.")
 
 
 if(CONFIG_CPLUSPLUS)
-  zephyr_linker_section(NAME ctors KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+  zephyr_linker_section(NAME ctors KVMA RAM_REGION LMA ROM_REGION NOINPUT)
   #
   # The compiler fills the constructor pointers table below,
   # hence symbol __CTOR_LIST__ must be aligned on word
@@ -77,7 +77,7 @@ endif()
 
 zephyr_linker_section(
   NAME app_shmem_regions
-  KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT
+  KVMA RAM_REGION LMA ROM_REGION NOINPUT
 )
 zephyr_linker_section_configure(
   SECTION app_shmem_regions
@@ -86,94 +86,94 @@ zephyr_linker_section_configure(
 )
 
 if(CONFIG_NET_SOCKETS)
-  zephyr_iterable_section(NAME net_socket_register KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME net_socket_register KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 
 if(CONFIG_NET_L2_PPP)
-  zephyr_iterable_section(NAME ppp_protocol_handler KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME ppp_protocol_handler KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
-zephyr_iterable_section(NAME bt_l2cap_fixed_chan KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+zephyr_iterable_section(NAME bt_l2cap_fixed_chan KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
 if(CONFIG_BT_BREDR)
-  zephyr_iterable_section(NAME bt_l2cap_br_fixed_chan KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_l2cap_br_fixed_chan KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
-zephyr_iterable_section(NAME bt_gatt_service_static KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+zephyr_iterable_section(NAME bt_gatt_service_static KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
 if(CONFIG_BT_MESH)
-  zephyr_iterable_section(NAME bt_mesh_subnet_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
-  zephyr_iterable_section(NAME bt_mesh_app_key_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_subnet_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_app_key_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
-  zephyr_iterable_section(NAME bt_mesh_hb_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_hb_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_BT_MESH_FRIEND)
-  zephyr_iterable_section(NAME bt_mesh_friend_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_friend_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_BT_MESH_LOW_POWER)
-  zephyr_iterable_section(NAME bt_mesh_lpn_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_lpn_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_BT_MESH_PROXY)
-  zephyr_iterable_section(NAME bt_mesh_proxy_cb KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME bt_mesh_proxy_cb KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_EC_HOST_CMD)
-  zephyr_iterable_section(NAME ec_host_cmd_handler KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME ec_host_cmd_handler KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_SETTINGS)
-  zephyr_iterable_section(NAME settings_handler_static KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME settings_handler_static KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
-zephyr_iterable_section(NAME k_p4wq_initparam KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+zephyr_iterable_section(NAME k_p4wq_initparam KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
 if(CONFIG_EMUL)
-  zephyr_linker_section(NAME emulators_section LMA ${ROM_REGION})
+  zephyr_linker_section(NAME emulators_section LMA ROM_REGION)
   zephyr_linker_section_configure(SECTION emulators_section INPUT ".emulators" KEEP SORT NAME)
 endif()
 
 if(CONFIG_DNS_SD)
-  zephyr_iterable_section(NAME dns_sd_rec KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+  zephyr_iterable_section(NAME dns_sd_rec KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 endif()
 
 if(CONFIG_PCIE)
-  zephyr_linker_section(NAME irq_alloc LMA ${ROM_REGION} NOINPUT)
+  zephyr_linker_section(NAME irq_alloc LMA ROM_REGION NOINPUT)
   zephyr_linker_section_configure(SECTION irq_alloc INPUT ".irq_alloc*" KEEP SORT NAME)
 endif()
 
-zephyr_linker_section(NAME log_strings KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME log_strings KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION log_strings INPUT ".log_strings*" KEEP SORT NAME)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_linker_section(NAME log_const KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME log_const KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION log_const INPUT ".log_const_*" KEEP SORT NAME)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_linker_section(NAME log_backends KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME log_backends KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION log_backends INPUT ".log_backends.*" KEEP)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_iterable_section(NAME shell KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+zephyr_iterable_section(NAME shell KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
-zephyr_linker_section(NAME shell_root_cmds KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME shell_root_cmds KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION shell_root_cmds INPUT ".shell_root_cmd_*" KEEP SORT NAME)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_linker_section(NAME font_entry KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME font_entry KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION font_entry INPUT "._cfb_font.*" KEEP SORT NAME)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_iterable_section(NAME tracing_backend KVMA ${RAM_REGION} LMA ${ROM_REGION} SUBALIGN 4)
+zephyr_iterable_section(NAME tracing_backend KVMA RAM_REGION LMA ROM_REGION SUBALIGN 4)
 
-zephyr_linker_section(NAME zephyr_dbg_info KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME zephyr_dbg_info KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION font_entry INPUT ".zephyr_dbg_info" KEEP)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
 
-zephyr_linker_section(NAME device_handles KVMA ${RAM_REGION} LMA ${ROM_REGION} NOINPUT)
+zephyr_linker_section(NAME device_handles KVMA RAM_REGION LMA ROM_REGION NOINPUT)
 zephyr_linker_section_configure(SECTION device_handles INPUT .__device_handles_pass1* KEEP SORT NAME PASS 1)
 zephyr_linker_section_configure(SECTION device_handles INPUT .__device_handles_pass2* KEEP SORT NAME PASS 2)
 # GROUP_ROM_LINK_IN(RAMABLE_REGION, ROMABLE_REGION)
