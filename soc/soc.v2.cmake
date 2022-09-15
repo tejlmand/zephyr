0b4b4e2911eed6abd9fe5421fe92e662139f075f
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2022-09-15 09:40:13 +0200

    kconfig: soc: adopt nRF52 SoC series to support SoC scheme v2
    This commit adopts SoC v2 to nRF52 SoC series.
    
    This ensures that when Board scheme v2 is used for an nRF52 SoC series,
    then all SoC selection is handled internally by Kconfig, and no
    selection / re-configuration is possible by end-user / through
    configuration files.
    
    SoC Scheme v1 is still supported for backward compatibility.
    
    The SoC scheme v1 defines a choice menu, into which the SoC config
    entries must be injected. This is done through the existing
    `soc/arm/nordic_nrf/Kconfig.soc`.
    
    SoC scheme v2 uses promptless entries, and in order to share the same
    Kconfig.series files, then prompt is made conditional.
    
    SoC scheme v2 requires the SoC to be self-contained, that is no
    references are allowed out-side the SoC Kconfig tree.
    
    To support both v1 and v2 scheme, then SoC names have been moved from
    defconfigs into Kconfig.soc.names, and a Kconfig.soc.common with high
    level nRF SoC identifiers. This allow tp share those settings between
    both schemes.
    
    To avoid visible prompts for nRF51, nRF53, and nRF91 series, when loaded
    into SoC scheme v2, then the prompt has been made conditional.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- soc/soc.v2.cmake
+++ soc/soc.v2.cmake
@@ -4,3 +4,4 @@
 
 # This file contains adding of hw model v2 SoC CMake lists.
 # Inclusion must be guarded on SoC name, SoC Family, or similar
+add_subdirectory_ifdef(CONFIG_SOC_FAMILY_NRF v2/nordic_nrf)
