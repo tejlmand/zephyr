04870f53ff9a9b415977037528e574033e1f2b33
Keith Packard <keithp@keithp.com>
2023-01-06 13:25:51 -0800

    toolchain/zephyr: Add TOOLCHAIN_HAS_PICOLIBC for sdk >= 0.16
    Zephyr SDK version 0.16 will include picolibc support; add a variable so
    that tests may check for it.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -4,4 +4,8 @@ include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
 
 set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
+if(SDK_VERSION VERSION_GREATER_EQUAL 0.16)
+  set(TOOLCHAIN_HAS_PICOLIBC ON CACHE BOOL "True if toolchain supports picolibc")
+endif()
+
 message(STATUS "Found toolchain: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
