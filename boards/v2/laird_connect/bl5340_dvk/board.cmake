cfc4dde9238ca491fd4c5054f79fd7039e8433d7
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-01-04 10:56:25 +0000

    boards: arm: bl5340_dvk: Convert to v2
    Converts the board to hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/v2/laird_connect/bl5340_dvk/board.cmake
+++ boards/v2/laird_connect/bl5340_dvk/board.cmake
@@ -2,21 +2,20 @@
 # Copyright (c) 2021 Laird Connectivity
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_BL5340_DVK_CPUAPP_NS)
+if("${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpuapp/ns")
   set(TFM_PUBLIC_KEY_FORMAT "full")
 endif()
 
-if(CONFIG_BOARD_BL5340_DVK_CPUAPP OR CONFIG_BOARD_BL5340_DVK_CPUAPP_NS)
-board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+if("${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpuapp" OR "${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpuapp/ns")
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+elseif("${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpunet")
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
 if(CONFIG_TFM_FLASH_MERGED_BINARY)
   set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 endif()
 
-if(CONFIG_BOARD_BL5340_DVK_CPUNET)
-board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
-endif()
-
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
