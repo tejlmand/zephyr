abc86c113b7f61124e2b14f5dd214c40d3c433ab
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-01-04 09:36:35 +0000

    boards: arm: raytac_mdbt53v_db_40_nrf5340: Convert to v2
    Converts the board to hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/v2/raytac/raytac_mdbt53v_db_40/board.cmake
+++ boards/v2/raytac/raytac_mdbt53v_db_40/board.cmake
@@ -1,12 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_RAYTAC_MDBT53V_DB_40_NRF5340_CPUAPP OR CONFIG_BOARD_RAYTAC_MDBT53V_DB_40_NRF5340_CPUAPP_NS)
+if("${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpuapp" OR "${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpuapp/ns")
   board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-endif()
-
-if(CONFIG_BOARD_RAYTAC_MDBT53V_DB_40_NRF5340_CPUNET)
+elseif("${BOARD_IDENTIFIER}" STREQUAL "/nrf5340/cpunet")
   board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
